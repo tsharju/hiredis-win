ce38ab6861cb473467ac3401b60ba26e6463674d
Teemu Harju <teemu@grandcrugames.com>
2014-03-06 16:02:04 +0200

    Fixed assertion that was caused by broken encoder since Windows does not support "%zu" string formatting.

--- hiredis/hiredis.c
+++ hiredis/hiredis.c
@@ -965,7 +965,11 @@ int redisFormatCommandArgv(char **target, int argc, const char **argv, const siz
     pos = sprintf(cmd,"*%d\r\n",argc);
     for (j = 0; j < argc; j++) {
         len = argvlen ? argvlen[j] : strlen(argv[j]);
+#ifdef WIN32
+        pos += sprintf(cmd+pos,"$%lu\r\n",len);
+#else
         pos += sprintf(cmd+pos,"$%zu\r\n",len);
+#endif
         memcpy(cmd+pos,argv[j],len);
         pos += len;
         cmd[pos++] = '\r';
